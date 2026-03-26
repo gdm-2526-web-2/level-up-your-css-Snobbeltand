0317b86c42ef538d292f5076dc51dbdf5581fc7f
Snobbeltand <elias.vdvelde@gmail.com>
2026-03-26 09:53:29 +0100

    update

--- dialog-popover/exercise/src/scripts/main.js
+++ dialog-popover/exercise/src/scripts/main.js
@@ -1,3 +1,21 @@
-import '../styles/main.css';
+import "../styles/main.css";
 
 /* showModal, closeModal is beschikbaar op dialog elementen */
+const modalTriggers = document.querySelectorAll("[data-modal-target]");
+
+modalTriggers.forEach((modalTrigger) => {
+  modalTrigger.addEventListener("click", () => {
+    const targetname = modalTrigger.getAttribute("data-modal-target");
+    console.log(modalTrigger, targetname);
+
+    const dialog = document.querySelector(`[data-modal="${targetname}"]`);
+    if (dialog) {
+      dialog.showModal();
+
+      const close = dialog.querySelector(".close");
+      close.addEventListener("click", () => {
+        dialog.close();
+      });
+    }
+  });
+});
--- media-queries/exercise/src/scripts/main.js
+++ media-queries/exercise/src/scripts/main.js
@@ -1,1 +1,8 @@
-import '../styles/main.css';
+import "../styles/main.css";
+
+const togglebutton = document.querySelector("#toggle-color-mode");
+const body = document.querySelector("body");
+
+togglebutton.addEventListener("click", () => {
+  body.classList.toggle("light-mode");
+});
